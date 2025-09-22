9a2fba73f80c235cc38557ff8737a7cf780550a7
ocrammer <ocrammer28@stsaviour.org>
2025-09-22 12:25:33 -0400

    print done

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 
 if __name__ == '__main__':
     print('welcome to computer science foundations!')
+
+ 
+    f = 2.5 
+
+    x = int(f) 
+
+    print(x)
+    # print(chr(sum(range(ord(min(str(not())))))))
+    
