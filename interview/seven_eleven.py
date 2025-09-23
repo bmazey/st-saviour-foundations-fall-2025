89ca591978c5daf86808bdf703caedb30fca0574
scarla <scorneille28@stsaviour.org>
2025-09-23 07:59:04 -0400

    coffee done

--- interview/seven_eleven.py
+++ interview/seven_eleven.py
@@ -8,6 +8,7 @@ def seven_eleven(number: int) -> str:
         - an empty string if the number is not a multiple of 7 or 11
     """
 
-    # TODO implement seven_eleven function
+    if number % 7 == 0:
+        return 'seven'
 
     return ''
