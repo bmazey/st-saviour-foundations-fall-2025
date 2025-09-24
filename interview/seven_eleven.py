85ae3014fe912cce63b2452d9374c0defcd6ae5b
laleshka <lalvarez28@stsaviour.org>
2025-09-24 08:10:03 -0400

    coffee done

--- interview/seven_eleven.py
+++ interview/seven_eleven.py
@@ -9,5 +9,7 @@ def seven_eleven(number: int) -> str:
     """
 
     # TODO implement seven_eleven function
-
-    return ''
+if number % 7 == 0 and number % 11 == 0 
+    return 'seveneleven'
+elif number % 7 == 0 
+ return " "
