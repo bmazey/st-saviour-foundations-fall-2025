af7aef7d5bde23edaa7aa12150a1d3409dbfe5ac
laleshka <lalvarez28@stsaviour.org>
2025-09-24 08:35:09 -0400

    lab 1 done

--- hello.py
+++ hello.py
@@ -3,13 +3,3 @@ if __name__ == '__main__':
 commit-tree
 
     print('welcome to computer science foundations!')
-
-
-f = 2.5
-
-x = int(f)
-
-print ( s + x )
-s = 'September'
-
-print(f)
--- interview/coffee.py
+++ interview/coffee.py
@@ -12,12 +12,12 @@ def classify(price: float) -> str:
 
     # TODO implement classify function
 
-  if price > 0 and price <= 5.0:
-    return 'dunkin donuts'
-  if price > 5.0 and price < 11.0:
-    return 'blank street'
-  if price >= 11.0 < 17.50:
-    return 'starbucks'
-  if price >= 17.50:
-    return 'cafe paradiso'
-  return 'error'
+    if price > 0 and price <= 5.0:
+      return 'dunkin donuts'
+    if price > 5.0 and price < 11.0:
+      return 'blank street'
+    if price >= 11.0 and price < 17.50:
+      return 'starbucks'
+    if price >= 17.50:
+      return 'cafe paradiso'
+    return 'error'
