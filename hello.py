358051027f0b2f0db0e00071ccf3038f9c316756
brandon mazey <brandon@brandons-MacBook-Air.local>
2025-10-05 09:16:49 -0400

    clearing

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 
 if __name__ == '__main__':
-
     # hello from my branch!
-
     print('welcome to computer science foundations!')
+
+    a = True
+    b = False
+
+    if a or b:
+        print('go pandas!')
+    else:
+        print('uh oh!')
+
+    # print(chr(sum(range(ord(min(str(not())))))))
