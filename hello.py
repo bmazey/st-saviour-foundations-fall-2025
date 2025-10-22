ace73078f81b7efa1037c4ddcc9f51d8d66c8a68
pdillon <pdillon28@stsaviour.org>
2025-10-22 08:00:09 -0400

    lab 2

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 
 if __name__ == '__main__':
     # hello from peyton!
-    print('welcome to computer science foundations!')
-
+    print('welcome to computer science foundation ) https://stsaviour.powerschool.com/guardian/scores.html?frn=00489684&begdate=09/03/2025&enddate=01/26/2026&fg=S1&schoolid=787644333
+          
+          
