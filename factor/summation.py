22f91431f8f87cbfdb91b4b46f0ae600cf5c75d3
aauxelus <aauxelus28@saintsaviour.org>
2025-10-22 08:21:35 -0400

    lab 2 done

--- factor/summation.py
+++ factor/summation.py
@@ -5,7 +5,10 @@ def summation(numbers: list[int]) -> int:
     and returns the sum of all numbers found within the list.
     ex: [2, 1, 3, 1, 0] -> 7
     """
-
-    # TODO
-
-    return 0
+    result = 0
+    i = 0
+    while i < len(numbers):
+        result = result + numbers[i]
+        i = i + 1
+    return result
+    
