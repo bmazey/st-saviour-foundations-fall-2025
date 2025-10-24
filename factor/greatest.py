5de73167f6ba080a4886c71bbf189eff0deb50f8
Molly <kaedeakamatsu2369420@gmail.com>
2025-10-24 13:35:07 -0400

    bonus

--- factor/greatest.py
+++ factor/greatest.py
@@ -4,8 +4,12 @@ def find_greatest(numbers: list[int]) -> int:
     find_greatest() is a function which accepts a list of integers
     and returns the greatest number found within the list.
         ex: [4, 7, 9, 11, 0] -> 11
-    """
+                                                                                                                                                                                                                                                                                                                                                                                                      n                                                                                                                               """
 
-    # TODO
+    greatest = numbers[0]  
+    for number in numbers:
+        if number > greatest:
+            greatest = number                                                                                                                                                                                        
 
-    return 0
+    return greatest                                                                                                                                                                                      
+  
--- factor/summation.py
+++ factor/summation.py
@@ -6,6 +6,12 @@ def summation(numbers: list[int]) -> int:
     ex: [2, 1, 3, 1, 0] -> 7
     """
 
-    # TODO
+    result = 0
+    
 
-    return 0
+    i = 0
+    while i < len(numbers):
+        result += numbers[i]
+        i += 1
+        
+    return result
