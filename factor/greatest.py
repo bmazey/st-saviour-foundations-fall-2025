5c859c55347bd8f863174c20ce92fb9b7ddb7dd1
loriann <loriann952@gmail.com>
2025-10-24 14:04:30 -0400

    lab 2 done

--- factor/greatest.py
+++ factor/greatest.py
@@ -6,6 +6,9 @@ def find_greatest(numbers: list[int]) -> int:
         ex: [4, 7, 9, 11, 0] -> 11
     """
 
-    # TODO
+    greatest = numbers[0]
+    for number in numbers:
+        if number > greatest:
+            greatest = number
 
-    return 0
+    return greatest
